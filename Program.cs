0a236b22a1ba79997fb09e4595cb658749f39f07
Hassan Allam <hassan309pro@gmail.com>
2025-06-21 02:19:54 +0300

    Errors Types

--- Program.cs
+++ Program.cs
@@ -11,6 +11,38 @@
             in some regions 
              */
             #endregion
+
+            #region Errors Types
+            /*
+              Syntax Error Like:
+            int Y = 5 // Missing semicolon ;
+            Syntax Error Being the Easier Error You Will Deal With
+            */
+
+            /*
+              Logic Error:
+             it being the most Complicated Error Because 
+            the Compiler does not show the error it happen because 
+            you do Some thing does not matching with the output you need 
+            like when You want to sum two numbers and you input'-' insteade of '+'
+             */
+
+            /*
+              Run Time Error :
+            That Happen in Run Time like :
+            You Divided Number By Zero 
+             */
+
+            /*
+              I/O Error :
+            That happen when there are a problem with I/O Oprations
+            Like : Try to access the file does not exist
+             */
+            #endregion
+
+            #region
+
+            #endregion
         }
     }
 }
